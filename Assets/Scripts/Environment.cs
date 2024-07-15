6afb1496e572d2ba62351f1ab9e356404c8a683b
unknown <drmatthewjdoyle@gmail.com>
2024-07-15 15:53:08 +0100

    introduce environment class

---
+++ Assets/Scripts/Environment.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Environment : MonoBehaviour
+{
+    [field: SerializeField]
+    public GameObject Goal { get; private set; }
+    
+    [field:SerializeField]
+    public Robot Robot { get; private set; }
+
+
+}
